2ea384080f5d1263faad927b6616640757fa8ec0
David Backeus <duztdruid@gmail.com>
2009-03-24 22:37:38 +0100

    Added 'time ago' style time formatting

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,5 +1,5 @@
 module CommentsHelper
-  def pretty_time(time)
+  def time_pretty(time)
     time.strftime("%Y-%m-%d %H:%M")
   end
 end
---
+++ app/helpers/time_helper.rb
@@ -0,0 +1,5 @@
+module TimeHelper
+  def time_ago(time)
+    time_ago_in_words(time) + " ago"
+  end
+end
