b2dad5632ca046ca12e429ab07b589c64a943a20
David Backeus <duztdruid@gmail.com>
2009-03-25 23:07:06 +0100

    Added some not logged in tests to recordings controller

--- spec/controllers/recordings_controller_spec.rb
+++ spec/controllers/recordings_controller_spec.rb
@@ -53,168 +53,194 @@ describe RecordingsController do
   
   end
   
-  describe "handling GET /recordings/new" do
-  
-    before(:each) do
-      log_in
-      get :new
-    end
+  logged_in do
     
-    it "should be successful" do
-      response.should be_success
-    end
-  
-    it "should assign a new recording for the view" do
-      assigns[:recording].attributes.should == current_user.recordings.build.attributes
-    end
-  end
-  
-  describe "handling GET /recordings/1/edit" do
-  
-    before(:each) do
-      log_in
-      @recording = Factory(:recording, :user => current_user)
-      get :edit, :id => @recording.id
-    end
-  
-    it "should be successful" do
-      response.should be_success
-    end
-    
-    it "should assign the recording for the view" do
-      assigns[:recording].should == @recording 
-    end
-  end
-  
-  describe "handling POST /recordings" do
-  
-    before(:each) do
-      log_in
-    end
-    
-    describe "with good attributes" do
-  
-      def do_post
-        post :create, :recording => {
-          :title => "Supermannen",
-          :length => "34.233",
-          :filename => "test.flv"
-        }
-      end
-  
-      it "should create a recording" do
-        lambda { do_post }.should change(Recording, :count).by(1)
-      end
-  
-      it "should redirect to the new recording" do
-        do_post
-        response.should redirect_to(recording_path(Recording.last))
-      end
-      
-    end
-    
-    describe "with bad attributes" do
-      
-      def do_post
-        post :create, :recording => {}
-      end
-      
-      it "should not create a recording" do
-        lambda { do_post }.should_not change(Recording, :count).by(1)
-      end
-      
-      it "should re-render 'new'" do
-        do_post
-        response.should render_template('new')
-      end
-      
-    end
-  end
-  
-  describe "handling PUT /recordings/1" do
-  
-    before(:each) do
-      log_in
-      @recording = Factory(:recording, :user => current_user)
-      @original_attributes = @recording.attributes
-    end
-  
-    describe "with good attributes" do
-  
+    describe "handling GET /recordings/new" do
       before(:each) do
-        @good_attributes = { :title => "New title", :description => "New description" }
+        get :new
       end
-  
-      def do_put
-        put :update, :id => @recording.id, :recording => @good_attributes
+    
+      it { should respond_with(:success) }
+      it { should assign_to(:recording).with_kind_of(Recording) }
+      it "should scope recording to logged in user" do
+        assigns(:recording).user.should == current_user
       end
-  
-      it "should update the found recording" do
-        @original_attributes.should_not include(*@good_attributes.values)
-        do_put
-        @recording.reload.attributes.values.should include(*@good_attributes.values)
-      end
-  
-      it "should assign the found recording for the view" do
-        do_put
-        assigns(:recording).should == @recording
-      end
-  
-      it "should redirect to the recording" do
-        do_put
-        response.should redirect_to(recording_url(@recording))
-      end
-  
     end
     
-    describe "with failed update" do
+    describe "handling GET /recordings/1/edit" do
+      before(:each) do
+        @recording = Factory(:recording, :user => current_user)
+        get :edit, :id => @recording.id
+      end
+      
+      it { should respond_with(:success) }
+      it { should assign_to(:recording).with(@recording) }
+    end
+
+    describe "handling POST /recordings" do
+
+      describe "with good attributes" do
+        
+        def do_post
+          post :create, :recording => {
+            :title => "Supermannen",
+            :length => "34.233",
+            :filename => "test.flv"
+          }
+        end
+
+        it "should create a recording" do
+          lambda { do_post }.should change(Recording, :count).by(1)
+        end
+
+        it "should redirect to the new recording" do
+          do_post
+          response.should redirect_to(recording_path(Recording.last))
+        end
+        
+      end
+
+      describe "with bad attributes" do
+        
+        def do_post
+          post :create, :recording => {}
+        end
+
+        it "should not create a recording" do
+          lambda { do_post }.should_not change(Recording, :count).by(1)
+        end
+
+        it "should re-render 'new'" do
+          do_post
+          response.should render_template('new')
+        end
+        
+      end
+    end
+
+    describe "handling PUT /recordings/1" do
       
       before(:each) do
-        @bad_attributes = { :title => nil, :description => 'This should not change' }
+        @recording = Factory(:recording, :user => current_user)
+        @original_attributes = @recording.attributes
       end
-      
-      def do_put
-        put :update, :id => @recording, :recording => @bad_attributes
+
+      describe "with good attributes" do
+        
+        before(:each) do
+          @good_attributes = { :title => "New title", :description => "New description" }
+        end
+
+        def do_put
+          put :update, :id => @recording.id, :recording => @good_attributes
+        end
+
+        it "should update the found recording" do
+          @original_attributes.should_not include(*@good_attributes.values)
+          do_put
+          @recording.reload.attributes.values.should include(*@good_attributes.values)
+        end
+
+        it "should assign the found recording for the view" do
+          do_put
+          assigns(:recording).should == @recording
+        end
+
+        it "should redirect to the recording" do
+          do_put
+          response.should redirect_to(recording_url(@recording))
+        end
+        
       end
-      
-      it "should re-render 'edit'" do
-        do_put
-        response.should render_template('edit')
+
+      describe "with failed update" do
+        
+        before(:each) do
+          @bad_attributes = { :title => nil, :description => 'This should not change' }
+        end
+
+        def do_put
+          put :update, :id => @recording, :recording => @bad_attributes
+        end
+
+        it "should re-render 'edit'" do
+          do_put
+          response.should render_template('edit')
+        end
+
+        it "should not update the recording" do
+          do_put
+          @recording.reload.attributes.values.should_not include(@bad_attributes)
+        end
+        
       end
+    end
+
+    describe "handling DELETE /recordings/1" do
       
-      it "should not update the recording" do
-        do_put
-        @recording.reload.attributes.values.should_not include(@bad_attributes)
+      before(:each) do
+        @recording = Factory(:recording, :user => current_user)
+      end
+
+      def do_delete
+        delete :destroy, :id => @recording.id
+      end
+
+      it "should destroy the found recording" do
+        Recording.find_by_id(@recording.id).should_not be_nil
+        lambda { do_delete }.should change(Recording, :count).by(-1)
+        Recording.find_by_id(@recording.id).should be_nil
+      end
+
+      it "should set the flash" do
+        do_delete
+        flash[:notice].should_not be_nil
+      end
+
+      it "should redirect to the user recordings list" do
+        do_delete
+        response.should redirect_to(user_recordings_url(@recording.user))
       end
       
     end
   end
   
-  describe "handling DELETE /recordings/1" do
-  
-    before(:each) do
-      log_in
-      @recording = Factory(:recording, :user => current_user)
-    end
-  
-    def do_delete
-      delete :destroy, :id => @recording.id
+  logged_out do
+    
+    describe "GET new" do
+      before(:each) do
+        get :new
+      end
+      it { should deny_access }
     end
     
-    it "should destroy the found recording" do
-      Recording.find_by_id(@recording.id).should_not be_nil
-      lambda { do_delete }.should change(Recording, :count).by(-1)
-      Recording.find_by_id(@recording.id).should be_nil
+    describe "GET edit" do
+      before(:each) do
+        get :edit, :id => "1"
+      end
+      it { should deny_access }
     end
     
-    it "should set the flash" do
-      do_delete
-      flash[:notice].should_not be_nil
+    describe "PUT update" do
+      before(:each) do
+        put :update, :id => "1"
+      end
+      it { should deny_access }
     end
-  
-    it "should redirect to the user recordings list" do
-      do_delete
-      response.should redirect_to(user_recordings_url(@recording.user))
+    
+    describe "POST create" do
+      before(:each) do
+        post :create, :id => "1"
+      end
+      it { should deny_access }
+    end
+    
+    describe "DELETE destroy" do
+      before(:each) do
+        delete :destroy, :id => "1"
+      end
+      it { should deny_access }
     end
   end
+  
 end
