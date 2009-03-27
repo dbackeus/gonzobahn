e77965e6b6b8a110ff6d4ea234c1b05b67ea79d0
David Backeus <duztdruid@gmail.com>
2009-03-27 20:08:14 +0100

    Minor refactoring to user controller specs

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -6,81 +6,89 @@ include AuthenticatedTestHelper
 
 describe UsersController do
 
-  it 'allows signup' do
-    lambda do
-      create_user
-      response.should be_redirect
-    end.should change(User, :count).by(1)
-  end
-
-  it 'signs up user in pending state' do
-    create_user
-    assigns(:user).reload
-    assigns(:user).should be_pending
-  end
-  
-  it 'signs up user with activation code' do
-    create_user
-    assigns(:user).reload
-    assigns(:user).activation_code.should_not be_nil
-  end
-
-  it 'requires login on signup' do
-    lambda do
-      create_user(:login => nil)
-      assigns[:user].errors.on(:login).should_not be_nil
-      response.should be_success
-    end.should_not change(User, :count)
-  end
-  
-  it 'requires password on signup' do
-    lambda do
-      create_user(:password => nil)
-      assigns[:user].errors.on(:password).should_not be_nil
-      response.should be_success
-    end.should_not change(User, :count)
-  end
-  
-  it 'requires password confirmation on signup' do
-    lambda do
-      create_user(:password_confirmation => nil)
-      assigns[:user].errors.on(:password_confirmation).should_not be_nil
-      response.should be_success
-    end.should_not change(User, :count)
-  end
-
-  it 'requires email on signup' do
-    lambda do
-      create_user(:email => nil)
-      assigns[:user].errors.on(:email).should_not be_nil
-      response.should be_success
-    end.should_not change(User, :count)
-  end
-  
-  it 'activates user' do
-    user = Factory.build(:user, :login => 'aaron', :password => 'test', :password_confirmation => 'test')
-    user.register!
-    user.save
-    user.reload
+  describe "POST create" do
     
-    User.authenticate('aaron', 'test').should be_nil
-    get :activate, :activation_code => user.activation_code
-    User.authenticate('aaron', 'test').should == user
-    flash[:notice].should_not be_nil
-    response.should redirect_to(user_recordings_path(user))
+    describe "with good parameters" do
+      before(:each) do
+        post :create, :user => { 
+          :login => 'quire', 
+          :email => 'quire@example.com',
+          :password => 'quire', 
+          :password_confirmation => 'quire' 
+        }
+      end
+      
+      it { should redirect_to(root_path) }
+      it { should set_the_flash.to(/thanks/i) }
+      
+      it "should create user" do
+        User.count.should == 1
+      end
+      
+      it "should create user in pending state" do
+        assigns(:user).reload.should be_pending
+      end
+      
+      it "should create user with activation code" do
+        assigns(:user).reload.activation_code.should_not be_nil
+      end
+    end
+
+    describe "with bad parameters" do
+      before(:each) do
+        post :create, :user => { 
+          :login => nil, 
+          :email => 'quire@example.com',
+          :password => 'quire', 
+          :password_confirmation => 'quire' 
+        }
+      end
+      
+      it { should_not set_the_flash }
+      it { should render_template(:new) }
+      
+      it "should not create user" do
+        User.count.should == 0
+      end
+    end
   end
   
-  it 'does not activate user with blank key' do
-    get :activate, :activation_code => ''
-    flash[:notice].should be_nil
+  describe "GET activate" do
+    it 'activates user' do
+      user = Factory.build(:user, :login => 'aaron', :password => 'test', :password_confirmation => 'test')
+      user.register!
+
+      User.authenticate('aaron', 'test').should be_nil
+      get :activate, :activation_code => user.activation_code
+      User.authenticate('aaron', 'test').should == user
+      flash[:notice].should_not be_nil
+      response.should redirect_to(user_recordings_path(user))
+    end
+
+    it 'does not activate user with blank key' do
+      get :activate, :activation_code => ''
+      flash[:notice].should be_nil
+    end
   end
   
-  def create_user(options = {})
-    post :create, :user => { 
-      :login => 'quire', 
-      :email => 'quire@example.com',
-      :password => 'quire', 
-      :password_confirmation => 'quire' 
-    }.merge(options)
+  logged_out do
+    
+    describe "GET edit" do
+      before(:each) do
+        get :edit, :id => "1"
+      end
+      
+      it { should deny_access }
+    end
+    
+    describe "PUT update" do
+      before(:each) do
+        put :update, :id => "1"
+      end
+      
+      it { should deny_access }
+    end
+    
   end
+  
 end
