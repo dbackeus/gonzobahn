6b74beea3feb798994e35fc12bb34ab8ce5a35fe
David Backeus <duztdruid@gmail.com>
2009-03-28 18:33:10 +0100

    Added load paths to I18n to be able to split up translations in multiple files. Added some initial translations.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  
+  def title_for(object, prefix)
+    "#{t(prefix)} #{object.class.human_name}"
+  end
+  
 end
--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,1 +1,2 @@
+I18n.load_path = Dir["#{RAILS_ROOT}/config/locales/*.{rb,yml}"]
 I18n.default_locale = :sv
