dcc358073bceb657824063e62f1ff49021fe7566
David Backeus <duztdruid@gmail.com>
2009-03-28 16:27:06 +0100

    Added a flash helper for embedding swf objects

---
+++ app/helpers/flash_helper.rb
@@ -0,0 +1,11 @@
+module FlashHelper
+  def swf_object(file, width, height)
+    file = "/flash/#{file}"
+    object = ""
+    object << "<object width='#{width}' height='#{height}'>"
+    object << "<param name='movie' value='#{file}'></param>"
+    object << "<param name='allowFullScreen' value='true'></param><param name='allowscriptaccess' value='always'></param>"
+    object << "<embed src='#{file}' type='application/x-shockwave-flash' allowscriptaccess='always' allowfullscreen='true' width='#{width}' height='#{height}'></embed>"
+    object << "</object>"
+  end
+end
