5c3e5c0810a91bacc5e459227f484d13c8baa695
David Backeus <duztdruid@gmail.com>
2009-03-31 16:00:02 +0200

    Broke out authentication and states from user model as concerns

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 require 'digest/sha1'
 class User < ActiveRecord::Base
-  # Virtual attribute for the unencrypted password
+
   attr_accessor :password
 
   validates_presence_of     :login, :email
@@ -12,58 +12,15 @@ class User < ActiveRecord::Base
   validates_length_of       :email,    :within => 3..100
   validates_uniqueness_of   :login, :email, :case_sensitive => false
   
-  before_save :encrypt_password
-  
   has_many :recordings
   has_many :comments
   
   # prevents a user from submitting a crafted form that bypasses activation
   # anything else you want your user to change should be added here.
   attr_accessible :login, :email, :password, :password_confirmation
-
-  acts_as_state_machine :initial => :pending
-  state :passive
-  state :pending, :enter => :make_activation_code
-  state :active,  :enter => :do_activate
-  state :suspended
-  state :deleted, :enter => :do_delete
-
-  event :register do
-    transitions :from => :passive, :to => :pending, :guard => Proc.new {|u| !(u.crypted_password.blank? && u.password.blank?) }
-  end
   
-  event :activate do
-    transitions :from => :pending, :to => :active 
-  end
-  
-  event :suspend do
-    transitions :from => [:passive, :pending, :active], :to => :suspended
-  end
-  
-  event :delete do
-    transitions :from => [:passive, :pending, :active, :suspended], :to => :deleted
-  end
-
-  event :unsuspend do
-    transitions :from => :suspended, :to => :active,  :guard => Proc.new {|u| u.activated_at.present? }
-    transitions :from => :suspended, :to => :pending, :guard => Proc.new {|u| u.activation_code.present? }
-    transitions :from => :suspended, :to => :passive
-  end
-
-  def self.find_by_login(login)
-    User.first :conditions => ["login like ?", login]
-  end
-
-  # Authenticates a user by their login name and unencrypted password.  Returns the user or nil.
-  def self.authenticate(login, password)
-    u = find_in_state :first, :active, :conditions => {:login => login} # need to get the salt
-    u && u.authenticated?(password) ? u : nil
-  end
-
-  # Encrypts some data with the salt.
-  def self.encrypt(password, salt)
-    Digest::SHA1.hexdigest("--#{salt}--#{password}--")
-  end
+  concerned_with :authentication
+  concerned_with :states
 
   def to_param
     login.downcase
@@ -77,69 +34,14 @@ class User < ActiveRecord::Base
     "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(email)}.jpg?d=#{SITE_HOST}/images/missing_avatar.gif"
   end
 
-  # Encrypts the password with the user salt
-  def encrypt(password)
-    self.class.encrypt(password, salt)
-  end
-
-  def authenticated?(password)
-    crypted_password == encrypt(password)
-  end
-
-  def remember_token?
-    remember_token_expires_at && Time.now.utc < remember_token_expires_at 
-  end
-
-  # These create and unset the fields required for remembering users between browser closes
-  def remember_me
-    remember_me_for 2.weeks
-  end
-
-  def remember_me_for(time)
-    remember_me_until time.from_now.utc
-  end
-
-  def remember_me_until(time)
-    self.remember_token_expires_at = time
-    self.remember_token            = encrypt("#{email}--#{remember_token_expires_at}")
-    save(false)
-  end
-
-  def forget_me
-    self.remember_token_expires_at = nil
-    self.remember_token            = nil
-    save(false)
-  end
-
   # Returns true if the user has just been activated.
   def recently_activated?
     @activated
   end
 
-  protected
-  # before filter 
-  def encrypt_password
-    return if password.blank?
-    self.salt = Digest::SHA1.hexdigest("--#{Time.now.to_s}--#{login}--") if new_record?
-    self.crypted_password = encrypt(password)
-  end
-    
+  private
   def password_required?
     crypted_password.blank? || !password.blank?
   end
   
-  def make_activation_code
-    self.deleted_at = nil
-    self.activation_code ||= Digest::SHA1.hexdigest( Time.now.to_s.split(//).sort_by {rand}.join )
-  end
-  
-  def do_delete
-    self.deleted_at = Time.now.utc
-  end
-
-  def do_activate
-    @activated = true
-    self.activated_at = Time.now.utc
-    self.deleted_at = self.activation_code = nil
-  end
 end
---
+++ app/models/user/states.rb
@@ -0,0 +1,46 @@
+class User < ActiveRecord::Base
+  acts_as_state_machine :initial => :pending
+  state :passive
+  state :pending, :enter => :make_activation_code
+  state :active,  :enter => :do_activate
+  state :suspended
+  state :deleted, :enter => :do_delete
+
+  event :register do
+    transitions :from => :passive, :to => :pending, :guard => Proc.new {|u| !(u.crypted_password.blank? && u.password.blank?) }
+  end
+  
+  event :activate do
+    transitions :from => :pending, :to => :active 
+  end
+  
+  event :suspend do
+    transitions :from => [:passive, :pending, :active], :to => :suspended
+  end
+  
+  event :delete do
+    transitions :from => [:passive, :pending, :active, :suspended], :to => :deleted
+  end
+
+  event :unsuspend do
+    transitions :from => :suspended, :to => :active,  :guard => Proc.new {|u| u.activated_at.present? }
+    transitions :from => :suspended, :to => :pending, :guard => Proc.new {|u| u.activation_code.present? }
+    transitions :from => :suspended, :to => :passive
+  end
+  
+  protected
+  def make_activation_code
+    self.deleted_at = nil
+    self.activation_code ||= Digest::SHA1.hexdigest( Time.now.to_s.split(//).sort_by {rand}.join )
+  end
+  
+  def do_delete
+    self.deleted_at = Time.now.utc
+  end
+
+  def do_activate
+    @activated = true
+    self.activated_at = Time.now.utc
+    self.deleted_at = self.activation_code = nil
+  end
+end
