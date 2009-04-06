c4e74f351f99e4562e6bba18016e6f1e1c288397
David Backeus <duztdruid@gmail.com>
2009-04-06 21:30:13 +0200

    Translated email subjects

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -2,15 +2,14 @@ class UserMailer < ActionMailer::Base
   def signup_notification(user)
     setup_email(user)
     
-    @subject    += 'Please activate your new account'
+    @subject    += t("user_mailer.signup_notification.subject")
     @body[:url]  = "http://#{SITE_HOST}/activate/#{user.activation_code}"
-  
   end
   
   def activation(user)
     setup_email(user)
     
-    @subject    += 'Your account has been activated!'
+    @subject    += t("user_mailer.activation.subject")
     @body[:url]  = "http://#{SITE_HOST}/"
   end
   
@@ -18,7 +17,7 @@ class UserMailer < ActionMailer::Base
   def setup_email(user)
     recipients user.email
     from "noreply@diktafonen.com"
-    subject "[Diktafonen] "
+    subject "[#{t("sitename")}] "
     @body[:user] = user
   end
 end
